bc578f0a34f34238375012ed9af0f44a49add3f0
Alberto Martinez <albertojmv@gmail.com>
2016-02-11 15:02:05 -0400

    Admin NavBar.

---
+++ resources/views/master/adminnavbar.blade.php
@@ -0,0 +1,39 @@
+<nav id="sidebar" role="navigation" data-step="2" data-intro="Template has &lt;b&gt;many navigation styles&lt;/b&gt;"
+                data-position="right" class="navbar-default navbar-static-side">
+            <div class="sidebar-collapse menu-scroll">
+                <ul id="side-menu" class="nav">
+                    
+                     <div class="clearfix"></div>
+                    <li><a href="{{URL::to('admin/dashboard')}}"><i class="fa fa-tachometer fa-fw">
+                        <div class="icon-bg bg-orange"></div>
+                    </i><span class="menu-title">Dashboard</span></a></li>
+                    <li><a href="{{URL::to('admin/payments')}}"><i class="fa fa-desktop fa-fw">
+                        <div class="icon-bg bg-pink"></div>
+                    </i><span class="menu-title">Pagos</span></a>
+                       
+                    </li>
+                    <li><a href="{{URL::to('admin/loans')}}"><i class="fa fa-send-o fa-fw">
+                        <div class="icon-bg bg-green"></div>
+                    </i><span class="menu-title">Préstamos</span></a>
+                       
+                    </li>
+                    <li><a href="{{URL::to('admin/customers')}}"><i class="fa fa-edit fa-fw">
+                        <div class="icon-bg bg-violet"></div>
+                    </i><span class="menu-title">Clientes</span></a>
+                      
+                    </li>
+                    <li><a href="{{URL::to('admin/users')}}"><i class="fa fa-th-list fa-fw">
+                        <div class="icon-bg bg-blue"></div>
+                    </i><span class="menu-title">Usuarios</span></a>
+                          
+                    </li>
+                    <li><a href="{{URL::to('admin/settings')}}"><i class="fa fa-database fa-fw">
+                        <div class="icon-bg bg-red"></div>
+                    </i><span class="menu-title">Configuración</span></a>
+                      
+                    </li>
+                                  
+       
+                </ul>
+            </div>
+        </nav>
